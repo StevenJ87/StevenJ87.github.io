9e387d7c8acd9294ab0174473c4d0748e01efdaf
Steven Johnson <sjohnson21960@gmail.com>
2020-08-26 09:45:05 -0500

    ProfileCard path update

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import ProfileCard from "../components"
+import ProfileCard from "../components/Profilecard"
 
 class Projects extends Component {
   render() {
