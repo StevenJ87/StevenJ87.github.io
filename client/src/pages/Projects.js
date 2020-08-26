b66e103a0f39816795c686c0b978cd10a7bff116
Steven Johnson <sjohnson21960@gmail.com>
2020-08-26 09:41:32 -0500

    added Profile card and background image

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
+import ProfileCard from "../components"
 
 class Projects extends Component {
   render() {
     return (
       <Container fluid>
         <Row>
-          
+          <ProfileCard/>
         </Row>
         <h1>Projects</h1>
       </Container>
