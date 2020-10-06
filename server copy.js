86beb08f556c79fa238841d8cc289d4cb44fac24
Steven Johnson <sjohnson21960@gmail.com>
2020-10-06 16:17:09 -0500

    refresh update trial 1

---
+++ server copy.js
@@ -0,0 +1,15 @@
+// const express = require("express");
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static("client/build"));
+// }
+
+// app.get('*', function (req,res){
+//     res.sendFile('./client/build/index.html');
+// })
+
+// app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
--- server.js
+++ server.js
@@ -1,15 +1,37 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const session = require("express-session");
+
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-}
+app.use(express.static("public"));
+app.use(express.static("client/build"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
-app.get('*', function (req,res){
-    res.sendFile('./client/build/index.html');
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+
+app.use("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+// Syncing our database and logging a message to the user upon success
+
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
