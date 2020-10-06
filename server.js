6690dc3b6fe00fb13c0103ebfc8a93bbb17add65
Steven Johnson <sjohnson21960@gmail.com>
2020-10-06 16:35:02 -0500

    refresh trial 3

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
-require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Creating express app and configuring middleware needed for authentication
-const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(express.static("client/build"));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
 
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-
-app.use("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+app.get('*', function (req,res){
+    res.sendFile('./client/build/index.html');
 })
 
-// Syncing our database and logging a message to the user upon success
-
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
